2442861c7a7b802d87083ab594a77126fad5f068
Yuhao <yban@usc.edu>
2022-07-21 20:13:55 -0700

    track

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,5 @@ export const NEARBY_SATELLITE = "rest/v1/satellite/above";
 export const BASE_URL = "";
 
 export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
